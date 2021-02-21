4a7c6a6a1805dd817cb6ce22c132b52583911f94
ArunTeltia <teltia.arun18@gmail.com>
2021-02-22 00:12:17 +0530

    Extracting Hello World App to render it on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import HelloWorld from 'components/apps/HelloWorld';
 import type { ReactElement } from 'react';
 import styled from 'styled-components';
 
@@ -8,5 +9,5 @@ const Title = styled.h1`
 `;
 
 export default function Home(): ReactElement {
-  return <Title>Hello, world!</Title>;
+  return <HelloWorld />;
 }
