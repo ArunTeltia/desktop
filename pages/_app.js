dbc7d6550cd17d0778cbef8a2b0bbdaefb83c6d2
ArunTeltia <teltia.arun18@gmail.com>
2021-02-21 13:03:21 +0530

    Added sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
+import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
