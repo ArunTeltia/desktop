3713e95054cafb701be868dc4274bb8885241127
ArunTeltia <teltia.arun18@gmail.com>
2021-02-21 21:15:29 +0530

    Added themeProvider to fix test

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -1,9 +1,15 @@
 import { render } from '@testing-library/react';
 import Index from 'pages/index';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from 'themes/default.json';
 
 test('renders index page', () => {
-  const { getByText } = render(<Index />);
-  const helloWorldElement = getByText('Hello World');
+  const { getByText } = render(
+    <ThemeProvider theme={defaultTheme}>
+      <Index />
+    </ThemeProvider>
+  );
+  const helloWorldElement = getByText('Hello, world!');
 
   expect(helloWorldElement).toBeInTheDocument();
 });
