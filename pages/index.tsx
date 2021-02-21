7b6060343465ce2083ebc601715df01bdec394bf
ArunTeltia <teltia.arun18@gmail.com>
2021-02-22 02:13:47 +0530

    Built process loader🐱‍🚀

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,6 @@
+import ProcessLoader from 'contexts/process';
 import type { ReactElement } from 'react';
-import styled from 'styled-components';
-import processDirectory from 'utils/processDirectory';
-
-const Title = styled.h1`
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: 50px;
-  text-align: center;
-`;
 
 export default function Home(): ReactElement {
-  return <processDirectory.HelloWorld.Component />;
+  return <ProcessLoader />;
 }
